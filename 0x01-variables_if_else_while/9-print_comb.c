3ca79c4e240a6bb2cf0ca2e1c179538c465a82fb
PAMMY100 <princesamuel636@gmail.com>
2023-12-16 11:46:57 -0800

    number seprated with comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - function that print numbers sepreated with comma
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
