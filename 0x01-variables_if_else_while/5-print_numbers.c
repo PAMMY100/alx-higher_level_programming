14b7d986325cb70f318834b8653e39c45b43567e
PAMMY100 <princesamuel636@gmail.com>
2023-12-16 10:28:45 -0800

    print number task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - function that print 1-9
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		printf("%d", x);
+	}
+	printf("\n");
+	return (0);
+}
