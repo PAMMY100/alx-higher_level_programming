405e02ab89df3c4f04743cbb64710198591ead31
PAMMY100 <princesamuel636@gmail.com>
2023-12-17 05:46:10 -0800

    task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints 3 digit
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i, p, j;
+
+	for (i = '0'; i < '9'; i++)
+	{
+	for (p = i + 1; p < '9'; p++)
+	{
+	for (j = p + 1; j < '9'; j++)
+	{
+	if (i != p && p != j)
+	{
+	putchar(i);
+	putchar(p);
+	putchar(j);
+	if (i == '7' && p == '8' && j == '9')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
