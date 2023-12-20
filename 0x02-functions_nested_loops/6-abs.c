c699562ad1e9dd11cffa509f0b4450e1ecf79504
PAMMY100 <princesamuel636@gmail.com>
2023-12-20 01:49:31 -0800

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - function that computes absolute value of int
+ * @a: function parameter
+ * Return: always 0
+ */
+
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		return (-a);
+	}
+	else
+	{
+		return (a);
+	}
+}
