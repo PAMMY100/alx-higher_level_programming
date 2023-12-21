21e8be1de939146ff0b16f888d4a1b373954fc38
PAMMY100 <princesamuel636@gmail.com>
2023-12-20 16:25:37 -0800

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,11 +10,28 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (; n <= 97; n++)
-			printf("%d, ", n);
+		for (; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+			}
+			else
+				printf("%d, ", n);
+		}
 	}
 	else
+	{
 		for (; n >= 98; n--)
-			printf("%d, ", n);
-	printf("98\n");
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+			}
+			else
+				printf("%d, ", n);
+		}
+	}
 }
