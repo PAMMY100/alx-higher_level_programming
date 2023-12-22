5dd8aff21808be09ba56e1c82fbb10bac73a3799
PAMMY100 <princesamuel636@gmail.com>
2023-12-22 03:13:27 -0800

    character task

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function that prints character
+ * @n: function parameter
+ */
+
+void print_diagonal(int n)
+{
+	int x, j;
+
+	for (x = 0; x < n; x++)
+	{
+		for (j = 0; j <= x; j++)
+		{
+			_putchar(' ');
+		}
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
