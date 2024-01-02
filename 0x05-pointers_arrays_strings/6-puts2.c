f1eb215592302ea8a093f0a127a86b09d4709aeb
PAMMY100 <princesamuel636@gmail.com>
2024-01-02 15:40:41 -0800

    print even number task

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prints other character
+ * @str: function parameter
+ * Return: always 0
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
