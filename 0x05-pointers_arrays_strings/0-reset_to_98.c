d2560f7515c9571f7b542fce9a8713b2fe43d8f2
PAMMY100 <princesamuel636@gmail.com>
2024-01-02 09:13:03 -0800

    main h file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - function that takes a pointer to an int
+ * and updates the value it points to to 98
+ * @n: parameter of the function
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
