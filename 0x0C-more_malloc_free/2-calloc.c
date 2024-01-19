b66ab35ab6bd25d82f9ed47bfc1ec03f9509d3c6
pammybreeze <adebayosamuel.olamilekan@gmail.com>
2024-01-19 19:09:01 +0100

    a function that allocates memory

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,2 +1,23 @@
 #include "main.h"
 
+/**
+ * *_calloc - function that allocate memory for an array
+ * @nmemb: 1st parameter
+ * @size: 2nd parameter
+ * Return: pointer allocated to memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	size_t i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = 0;
+	return (p);
+}
