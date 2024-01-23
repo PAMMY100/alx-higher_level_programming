c941b064447f24844664e490a2832acf87290645
pammybreeze <adebayosamuel.olamilekan@gmail.com>
2024-01-23 13:05:30 +0100

    dog-h task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - short description
+ * @name: first member
+ * @age: second member
+ * @owner: Third member
+ * Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
