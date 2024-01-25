2479c64dc119f097455aa12d65bfc4550ed8d461
pammybreeze <adebayosamuel.olamilekan@gmail.com>
2024-01-25 22:34:09 +0100

    print name file corrected

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,6 +4,7 @@
  * print_name - function that prints a name
  * @name: function parameter of the name
  * @f: function pointer
+ * Return: nothing
  */
 
 void print_name(char *name, void (*f)(char *))
