b3d343db1690fadf3e51519c3dd454e1b185502f
PAMMY100 <princesamuel636@gmail.com>
2024-03-13 06:17:19 -0700

    quick fix

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 for i in range(100):
-<<<<<<< HEAD
     if i == 99:
         print("{}".format(i))
     else:
         print("{:02d}, ".format(i), end='')
-=======
-    if i < 99:
-        print("{:02d}".format(i), end=', ')
-    else:
-        print("{:02d}\n".format(i))
->>>>>>> afd53f7c9083fa78dff4abb422c0155e4421cafd
+
