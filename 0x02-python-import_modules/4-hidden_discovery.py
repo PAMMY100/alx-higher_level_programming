1260b51610820cb959dc14abfdedea5693b0a611
pammybreeze <princesamuel636@gmail.com>
2024-03-17 23:57:09 +0100

    print names defined by the compiled module

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import dis
+
+def print_names(filename):
+    with open(filename, 'rb') as file:
+        code = file.read()
+        instructions = dis.get_instructions(code)
+        names = set()
+        for instruct in instructions:
+            if instruct.opname == 'LOAD_NAME' and not instruct.argval.startswith('__'):
+                names.add(instruct.argval)
+
+    for name in sorted(names):
+        print(name)
+
+if __name__ == "__main__":
+    print_names("hidden_4.pyc")
