2e8ef527c397f36364fdb8fa4239beb57ae9bb50
pammybreeze <princesamuel636@gmail.com>
2024-03-18 19:18:43 +0100

    hidden discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 import dis
-
 def print_names(filename):
     with open(filename, 'rb') as file:
         code = file.read()
@@ -9,9 +8,7 @@ def print_names(filename):
         for instruct in instructions:
             if instruct.opname == 'LOAD_NAME' and not instruct.argval.startswith('__'):
                 names.add(instruct.argval)
-
     for name in sorted(names):
         print(name)
-
 if __name__ == "__main__":
     print_names("hidden_4.pyc")
