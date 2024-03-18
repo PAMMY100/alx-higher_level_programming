367fb0fece5bbe670b4b07ca59c3f143cb559a89
pammybreeze <princesamuel636@gmail.com>
2024-03-18 19:00:23 +0100

    quick fix for 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    num_args = len(argv) - 1
-    if num_args < 0:
-        print("{} arguments.".format(num_args)
-    elif num_args == 1:
-    print("{} argument:".format(num_args))
+    x = len(argv) - 1
+    if x < 0:
+        print("{} arguments.".format(x)
+    elif x == 1:
+        print("{} argument:".format(x))
     else:
-        print("{} arguments:".format(num_args))
+        print("{} arguments:".format(x))
    
-    for i in range(num_args):
+    for i in range(x):
         print("{}: {:s}".format(i + 1, argv[i + 1]))
