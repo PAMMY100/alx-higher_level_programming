2a7e3f07f5f4ba18bf30424d7654c65ca2a10fad
pammybreeze <princesamuel636@gmail.com>
2024-03-18 19:25:37 +0100

    a program that prints all the names defined by compiled module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-import dis
-def print_names(filename):
-    with open(filename, 'rb') as file:
-        code = file.read()
-        instructions = dis.get_instructions(code)
-        names = set()
-        for instruct in instructions:
-            if instruct.opname == 'LOAD_NAME' and not instruct.argval.startswith('__'):
-                names.add(instruct.argval)
-    for name in sorted(names):
-        print(name)
 if __name__ == "__main__":
-    print_names("hidden_4.pyc")
+    from hidden_4 import *
+    allf = dir()
+    for i in range(0, len(allf)):
+        if allf[i][:2] != "__":
+            print("{:s}".format(allf[i]))
